7942df11063a3d4753208a132e431cac09c1b00f
Nikolay <157130401678@mail.ru>
2021-12-20 16:28:20 +0300

    11th HW

--- src/main/java/com/example/gbspringlesson11/repositories/UserRepository.java
+++ src/main/java/com/example/gbspringlesson11/repositories/UserRepository.java
@@ -1,12 +1,13 @@
 package com.example.gbspringlesson11.repositories;
 
 import com.example.gbspringlesson11.entities.User;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long>, JpaSpecificationExecutor<User> {
     Optional<User> findByUsername(String username);
 }
